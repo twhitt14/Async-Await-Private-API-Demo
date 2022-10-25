ecfb10a4cb648551b90848cbae209d92fa22abfb
Trevor Whittingham <trevorw@ziprecruiter.com>
2022-10-25 15:50:13 -0600

    Initial Commit

---
+++ Async Await Private API Demo/Async_Await_Private_API_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_Private_API_DemoApp.swift
+//  Async Await Private API Demo
+//
+//  Created by Trevor Whittingham on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_Private_API_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
